e5609d6cc43cfa211a44f987a744883c43cf3d23
PWalis <pwilky1016@gmail.com>
2019-05-29 13:41:33 -0700

    Added the setup to upload to test pypi

---
+++ Lambdata_PWalis/testing_code.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+df = df.DataFrame({'Ones':np.ones(30)})
+df
---
+++ setup.py
@@ -0,0 +1,12 @@
+""" lambdata - a collection of Data Science helper functions
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
