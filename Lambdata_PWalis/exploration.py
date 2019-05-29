a2edc08ddb417476983c4287990ec31726a6bb99
PWalis <pwilky1016@gmail.com>
2019-05-29 14:46:44 -0700

    added new functions and renamed file

---
+++ Lambdata_PWalis/exploration.py
@@ -0,0 +1,19 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+def values(x):
+    # Given a dataframe this function will print out all of the columns values counts
+    for column in x.columns:
+        print(x[column].value_counts())
+
+def cardinality_finder(df, threshold, dtype):
+    # Given a DataFrame and an int this function will print the features below the threshold and display 
+    # the cardinality along with the dtype, give a list of dtypes to check if more than one is desired.
+    cf = df.select_dtypes(include=dtype)
+    for column in cf.columns:
+        data_type = cf[column].dtype
+        unique = cf[column].unique()
+        length = len(unique)
+        if (length < threshold):
+            print(column, length, data_type)
